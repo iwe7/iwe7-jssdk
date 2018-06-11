b61dd8b21803ed3aecddfc329a05b2ee30ffa35b
iwe7 <admin@meepo.com.cn>
2018-06-11 12:18:45 +0800

    播放语音封装

--- readme.md
+++ readme.md
@@ -5,8 +5,16 @@
 | loadObservable | 加载cdn上得jssd,远程获取jssdk初始化配置，并初始化jssdk |
 
 
-### Iwe7JssdkRecordService
+### Iwe7JssdkRecordService 录音封装
 | 方法     | 说明                         |
 |--------|----------------------------|
 | record | 开始录音，监听录音完成或停止录音，返回localId |
 | stop   | 停止录音                       |
+
+
+### Iwe7JssdkVoiceService 播放语音封装
+| 方法    | 说明                                          |
+|-------|---------------------------------------------|
+| play  | 播放，当播放完成时返回`end`，当暂停时放回`pause`,当停止播放时`stop` |
+| stop  | 停止                                          |
+| pause | 暂停                                          |
--- src/lib/iwe7-jssdk-voice.service.ts
+++ src/lib/iwe7-jssdk-voice.service.ts
@@ -1,22 +1,37 @@
-import { merge } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
+import { merge, Observable } from 'rxjs';
+import { switchMap, map } from 'rxjs/operators';
 import { Iwe7JssdkService } from './iwe7-jssdk.service';
 import { Injectable } from '@angular/core';
 
 @Injectable()
 export class Iwe7JssdkVoiceService {
-
+    localId: string;
     constructor(
         public iwe7Jssdk: Iwe7JssdkService
     ) { }
 
-    play(localId: string) {
+    play(localId: string = this.localId): Observable<'end' | 'stop' | 'pause' | string> {
+        this.localId = localId;
         return this.iwe7Jssdk.playVoice(localId).pipe(
             switchMap(res => {
                 return merge(
-                    this.iwe7Jssdk.stopVoice()
+                    // 当播放结束时
+                    this.iwe7Jssdk.onVoicePlayEnd().pipe(map(res => 'end')),
+                    // 停止播放
+                    this.iwe7Jssdk.getCyc('stopVoice').pipe(map(res => 'stop')),
+                    // 暂停播放
+                    this.iwe7Jssdk.getCyc('pauseVoice').pipe(map(res => 'pause'))
                 );
             })
         );
     }
+    // 停止播放
+    stop(localId: string = this.localId) {
+        this.iwe7Jssdk.stopVoice(localId).subscribe();
+    }
+    // 暂停播放
+    pause(localId: string = this.localId) {
+        this.iwe7Jssdk.pauseVoice(localId).subscribe();
+    }
+
 }
