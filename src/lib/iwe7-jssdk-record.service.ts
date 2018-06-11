43dfb84d18ddae02d68e445f7e2b336bda757a76
iwe7 <admin@meepo.com.cn>
2018-06-11 21:40:40 +0800

    录音增加调试模式

--- src/lib/iwe7-jssdk-record.service.ts
+++ src/lib/iwe7-jssdk-record.service.ts
@@ -1,18 +1,26 @@
-import { Observable, merge } from 'rxjs';
+import { Observable, merge, timer } from 'rxjs';
 import { Iwe7JssdkService } from './iwe7-jssdk.service';
-import { Injectable } from '@angular/core';
-import { takeUntil, switchMap, take, tap } from 'rxjs/operators';
+import { Injectable, isDevMode } from '@angular/core';
+import { takeUntil, switchMap, take, tap, map } from 'rxjs/operators';
 
 @Injectable({
     providedIn: 'root'
 })
 export class Iwe7JssdkRecordService {
-
+    dev: boolean = isDevMode();
     constructor(
         public iwe7Jssdk: Iwe7JssdkService
     ) { }
     // 开始录音
     start(): Observable<string> {
+        if (this.dev) {
+            setTimeout(() => {
+                this.iwe7Jssdk.setCyc('stopRecord', true);
+            }, 5000);
+        }
+        setTimeout(() => {
+            this.iwe7Jssdk.getCyc('startRecord', true);
+        }, 0);
         return this.iwe7Jssdk.startRecord().pipe(
             switchMap(res => {
                 return merge(
@@ -28,6 +36,11 @@ export class Iwe7JssdkRecordService {
     }
     // 开始录音，录音结束时上传
     startAndUpload(): Observable<string> {
+        if (this.dev) {
+            setTimeout(() => {
+                this.iwe7Jssdk.setCyc('uploadVoice', 'serviceId');
+            }, 500);
+        }
         return this.start().pipe(
             switchMap(localId =>
                 this.iwe7Jssdk.uploadVoice(localId)
@@ -35,10 +48,21 @@ export class Iwe7JssdkRecordService {
         );
     }
     upload(localId: string): Observable<string> {
+        if (this.dev) {
+            setTimeout(() => {
+                // 开发者模式下，延迟2秒返回serveId
+                this.iwe7Jssdk.setCyc('uploadVoice', 'serveId');
+            }, 2000);
+        }
         return this.iwe7Jssdk.uploadVoice(localId);
     }
     // 停止录音
     stop(): void {
+        if (this.dev) {
+            setTimeout(() => {
+                this.iwe7Jssdk.setCyc('stopRecord', 'localId');
+            }, 0);
+        }
         this.iwe7Jssdk.stopRecord().subscribe();
     }
 }
