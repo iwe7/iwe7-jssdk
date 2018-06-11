0cff4a1e0b06bfa649dc616b2e32a9cbf47c0291
iwe7 <admin@meepo.com.cn>
2018-06-11 13:18:45 +0800

    修复导出问题

--- src/public_api.ts
+++ src/public_api.ts
@@ -1,1 +1,5 @@
 export * from './lib/iwe7-jssdk.service';
+export * from './lib/iwe7-jssdk-voice.service';
+export * from './lib/iwe7-jssdk-image.service';
+export * from './lib/iwe7-jssdk-record.service';
+export * from './lib/iwe7-jssdk-share.service';
